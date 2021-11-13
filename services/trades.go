1a0e5e2fb099562309460ff52c9b05df3ff62d2d
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-14 02:30:01 +0900

    add tradable flow

--- services/trades.go
+++ services/trades.go
@@ -19,6 +19,17 @@ func StableTokens(wg *sync.WaitGroup, pairs utils.Pairs) {
 	}
 }
 
+func TradableTokens(wg *sync.WaitGroup, pairs utils.Pairs) {
+	defer wg.Done()
+
+	for _, item := range pairs.Data.Pairs {
+		c := make(chan string, 1)
+		fmt.Println(item.Id)
+		go utils.Post(c, "swaps", item.Id)
+		tradableToken(c, item.Id)
+	}
+}
+
 func stableToken(pings <-chan string, id string) {
 	var swaps utils.Swaps
 	msg := <-pings
@@ -29,9 +40,23 @@ func stableToken(pings <-chan string, id string) {
 		last, _ := priceOfSwap(swaps.Data.Swaps[0])
 		_, _, period := periodOfSwaps(swaps)
 
-		if (max-min)/last > 0.1 && period < 6 {
+		if (max-min)/last < 0.1 && period > 24 {
 			// target <- id
-		} else if (max-min)/last < 0.1 && period > 24 {
+		}
+	}
+}
+
+func tradableToken(pings <-chan string, id string) {
+	var swaps utils.Swaps
+	msg := <-pings
+	json.Unmarshal([]byte(msg), &swaps)
+
+	if len(swaps.Data.Swaps) > 0 {
+		min, max, _, _, _, _ := minMax(swaps)
+		last, _ := priceOfSwap(swaps.Data.Swaps[0])
+		_, _, period := periodOfSwaps(swaps)
+
+		if (max-min)/last > 0.1 && period < 6 {
 			// target <- id
 		}
 	}
