3b46f12475a5b2470559bd34bcb473d54a557765
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-28 23:00:46 +0900

    clean up code base

--- pages/track.go
+++ pages/track.go
@@ -14,7 +14,6 @@ import (
 	uniswap "github.com/hirokimoto/uniswap-api"
 	unitrade "github.com/hirokimoto/uniswap-api/swap"
 	unitrades "github.com/hirokimoto/uniswap-api/swaps"
-	"github.com/leekchan/accounting"
 	"github.com/uniswap-auto-gui/data"
 	"github.com/uniswap-auto-gui/services"
 )
@@ -24,7 +23,7 @@ func trackScreen(_ fyne.Window) fyne.CanvasObject {
 	var activePair string
 	var oldPrices = map[string]float64{}
 
-	money := accounting.Accounting{Symbol: "$", Precision: 6}
+	// money := accounting.Accounting{Symbol: "$", Precision: 6}
 	trades := map[string]uniswap.Swaps{}
 
 	pairs := data.ReadTrackPairs()
@@ -68,135 +67,108 @@ func trackScreen(_ fyne.Window) fyne.CanvasObject {
 		},
 	)
 
-	leftList := widget.NewListWithData(pairsdata,
+	table := widget.NewTable(
+		func() (int, int) { return len(pairs) + 1, 7 },
 		func() fyne.CanvasObject {
-			left := container.NewHBox(widget.NewHyperlink("DEX", parseURL("https://github.com/hirokimoto")), widget.NewLabel("token"), widget.NewLabel("price"), widget.NewLabel("change"), widget.NewLabel("duration"))
-			right := container.NewHBox(widget.NewButton("#", nil), widget.NewButton("-", nil))
-			return container.NewBorder(nil, nil, left, right)
+			return widget.NewLabel("Cell 000, 000")
 		},
-		func(item binding.DataItem, obj fyne.CanvasObject) {
-			s := item.(binding.String)
+		func(id widget.TableCellID, cell fyne.CanvasObject) {
+			pair := pairs[id.Row]
+			swaps := trades[pair]
 
-			left := obj.(*fyne.Container).Objects[0].(*fyne.Container)
-			dex := left.Objects[0].(*widget.Hyperlink)
-			pair, _ := s.Get()
-			url := fmt.Sprintf("https://www.dextools.io/app/ether/pair-explorer/%s", pair)
-			dex.SetURL(parseURL(url))
+			n := unitrade.Name(swaps.Data.Swaps[0])
+			p, _ := unitrade.Price(swaps.Data.Swaps[0])
+			_, c := unitrades.WholePriceChanges(swaps)
+			_, _, d := unitrades.Duration(swaps)
 
-			label := left.Objects[1].(*widget.Label)
-			price := left.Objects[2].(*widget.Label)
-			change := left.Objects[3].(*widget.Label)
-			duration := left.Objects[4].(*widget.Label)
+			label := cell.(*widget.Label)
+			switch id.Col {
+			case 0:
+				label.SetText(fmt.Sprintf("%d", id.Row+1))
+			case 1:
+				label.SetText(n)
+			case 2:
+				label.SetText(fmt.Sprintf("%f", p))
+			case 3:
+				label.SetText(fmt.Sprintf("%f", c))
+			case 4:
+				label.SetText(fmt.Sprintf("%f", d))
+			default:
+				label.SetText(fmt.Sprintf("Cell %d, %d", id.Row+1, id.Col+1))
+			}
+		})
+	table.SetColumnWidth(0, 34)
+	table.SetColumnWidth(1, 102)
 
-			right := obj.(*fyne.Container).Objects[1].(*fyne.Container)
-			btnSettings := right.Objects[0].(*widget.Button)
-			btnSettings.OnTapped = func() {
-				w := fyne.CurrentApp().NewWindow("Settings")
+	// leftList.OnSelected = func(id widget.ListItemID) {
+	// 	activePair, _ = pairsdata.GetValue(id)
+	// 	selected = trades[activePair]
+	// 	rightList.Refresh()
+	// }
+
+	listPanel := container.NewBorder(nil, control, nil, nil, table)
+
+	var swaps uniswap.Swaps
+	cc := make(chan string, 1)
+
+	go func() {
+		for _, pair := range pairs {
+			go uniswap.SwapsByCounts(cc, 10, pair)
+
+			msg := <-cc
+			json.Unmarshal([]byte(msg), &swaps)
+
+			if len(swaps.Data.Swaps) == 0 {
+				time.Sleep(time.Second * 5)
+				continue
+			}
+			if len(trades[pair].Data.Swaps) > 0 &&
+				(trades[pair].Data.Swaps[0].Id == swaps.Data.Swaps[0].Id ||
+					trades[pair].Data.Swaps[0].Timestamp > swaps.Data.Swaps[0].Timestamp) {
+				continue
+			}
+
+			n := unitrade.Name(swaps.Data.Swaps[0])
+			p, _ := unitrade.Price(swaps.Data.Swaps[0])
+			_, c := unitrades.WholePriceChanges(swaps)
+			_, _, d := unitrades.Duration(swaps)
+
+			if activePair == pair &&
+				selected.Data.Swaps[0].Id != swaps.Data.Swaps[0].Id {
+				selected = swaps
+				rightList.Refresh()
+			}
+			trades[pair] = swaps
+
+			if p != oldPrices[pair] {
+				t := time.Now()
+				message := fmt.Sprintf("%s: %f %f %f", n, p, c, d)
+				title := "Priced Up!"
+				if c < 0 {
+					title = "Priced Down!"
+				}
+				link := fmt.Sprintf("https://www.dextools.io/app/ether/pair-explorer/%s", pair)
 
 				min, max := data.ReadMinMax(pair)
-				mindata := binding.BindFloat(&min)
-				minLabel := widget.NewLabel("Minimum")
-				minEntry := widget.NewEntryWithData(binding.FloatToString(mindata))
-				minPanel := container.NewGridWithColumns(2, minLabel, minEntry)
 
-				maxdata := binding.BindFloat(&max)
-				maxLabel := widget.NewLabel("Maximum")
-				maxEntry := widget.NewEntryWithData(binding.FloatToString(maxdata))
-				maxPanel := container.NewGridWithColumns(2, maxLabel, maxEntry)
-
-				btnSave := widget.NewButton("Save", func() {
-					data.SaveTrackSettings(pair, min, max)
-				})
-
-				settingsPanel := container.NewVBox(minPanel, maxPanel, btnSave)
-				w.SetContent(settingsPanel)
-
-				w.Resize(fyne.NewSize(340, 180))
-				w.SetFixedSize(true)
-				w.Show()
-			}
-			btnRemove := right.Objects[1].(*widget.Button)
-			btnRemove.OnTapped = func() {
-
-			}
-
-			var swaps uniswap.Swaps
-			cc := make(chan string, 1)
-
-			go func() {
-				for {
-					go uniswap.SwapsByCounts(cc, 10, pair)
-
-					msg := <-cc
-					json.Unmarshal([]byte(msg), &swaps)
-
-					if len(swaps.Data.Swaps) == 0 {
-						time.Sleep(time.Second * 5)
-						continue
-					}
-					if len(trades[pair].Data.Swaps) > 0 &&
-						(trades[pair].Data.Swaps[0].Id == swaps.Data.Swaps[0].Id ||
-							trades[pair].Data.Swaps[0].Timestamp > swaps.Data.Swaps[0].Timestamp) {
-						continue
-					}
-
-					n := unitrade.Name(swaps.Data.Swaps[0])
-					p, _ := unitrade.Price(swaps.Data.Swaps[0])
-					_, c := unitrades.WholePriceChanges(swaps)
-					_, _, d := unitrades.Duration(swaps)
-
-					label.SetText(n)
-					_price := money.FormatMoney(p)
-					price.SetText(_price)
-					_change := money.FormatMoney(c)
-					change.SetText(_change)
-					_duration := fmt.Sprintf("%.2f hours", d)
-					duration.SetText(_duration)
-
-					if activePair == pair &&
-						selected.Data.Swaps[0].Id != swaps.Data.Swaps[0].Id {
-						selected = swaps
-						rightList.Refresh()
-					}
-					trades[pair] = swaps
-
-					if p != oldPrices[pair] {
-						t := time.Now()
-						message := fmt.Sprintf("%s: %f %f %f", n, p, c, d)
-						title := "Priced Up!"
-						if c < 0 {
-							title = "Priced Down!"
-						}
-						link := fmt.Sprintf("https://www.dextools.io/app/ether/pair-explorer/%s", pair)
-
-						min, max := data.ReadMinMax(pair)
-
-						if p < min {
-							title = fmt.Sprintf("Warning Low! Watch %s", n)
-						}
-						if p > max {
-							title = fmt.Sprintf("Warning High! Watch %s", n)
-						}
-
-						services.Alert(title, message, link, gosxnotifier.Morse)
-
-						fmt.Println(".")
-						fmt.Println(t.Format("2006/01/02 15:04:05"), ": ", n, p, c, d)
-						fmt.Println(".")
-					}
-					oldPrices[pair] = p
-
-					time.Sleep(time.Second * 1)
+				if p < min {
+					title = fmt.Sprintf("Warning Low! Watch %s", n)
 				}
-			}()
-		})
+				if p > max {
+					title = fmt.Sprintf("Warning High! Watch %s", n)
+				}
 
-	leftList.OnSelected = func(id widget.ListItemID) {
-		activePair, _ = pairsdata.GetValue(id)
-		selected = trades[activePair]
-		rightList.Refresh()
-	}
+				services.Alert(title, message, link, gosxnotifier.Morse)
 
-	listPanel := container.NewBorder(nil, control, nil, nil, leftList)
+				fmt.Println(".")
+				fmt.Println(t.Format("2006/01/02 15:04:05"), ": ", n, p, c, d)
+				fmt.Println(".")
+			}
+			oldPrices[pair] = p
+
+			time.Sleep(time.Second * 1)
+		}
+	}()
+
 	return container.NewHSplit(listPanel, rightList)
 }
