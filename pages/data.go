4c901def6410fbd335ea3b49da343fbb27c90404
Hiroki Moto <hiroki.moto.pro@gmail.com>
2021-11-30 13:19:39 +0900

    clean up code base

--- pages/data.go
+++ pages/data.go
@@ -14,16 +14,12 @@ var (
 	// Pages defines the metadata for each page
 	Pages = map[string]Page{
 		"welcome": {"Welcome", "", welcomeScreen},
-		"coins":   {"Coins", "", coinsScreen},
 		"track":   {"Track", "", trackScreen},
 		"trades":  {"Trades", "", tradesScreen},
 	}
 
 	// PageIndex  defines how our pages should be laid out in the index tree
 	PageIndex = map[string][]string{
-		"":            {"welcome", "coins", "track", "trades"},
-		"collections": {"list", "table", "tree"},
-		"containers":  {"apptabs", "border", "box", "center", "doctabs", "grid", "scroll", "split"},
-		"widgets":     {"accordion", "button", "card", "entry", "form", "input", "progress", "text", "toolbar"},
+		"": {"welcome", "track", "trades"},
 	}
 )
